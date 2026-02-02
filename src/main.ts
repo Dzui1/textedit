4eea406f95e18884ca28ad0be2fbced773314977
Dzui1 <buithaiduy2006@gmail.com>
2026-02-02 11:18:08 -0500

    First commit

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import "./style.css";
+
+const textbox = document.getElementById("textbox") as HTMLTextAreaElement;
+
+textbox.value = localStorage.getItem("key") ?? "";
+
+textbox.addEventListener("keydown", (event) => {
+  if (event.key == "Tab") {
+    event.preventDefault();
+    const start = textbox.selectionStart;
+    const end = textbox.selectionEnd;
+
+    // set textarea value to: text before caret + tab + text after caret
+    textbox.value =
+      textbox.value.substring(0, start) + "\t" + textbox.value.substring(end);
+
+    // put caret at right position again
+    textbox.selectionStart = textbox.selectionEnd = start + 1;
+  }
+});
+
+textbox.addEventListener("input", () => {
+  localStorage.setItem("key", textbox.value);
+});
