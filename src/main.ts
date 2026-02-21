5b7deedea8474d90012dcc1544eedccbab790848
Dzui1 <buithaiduy2006@gmail.com>
2026-02-21 18:27:45 -0500

    add ts fallback to work on firefox

--- src/main.ts
+++ src/main.ts
@@ -22,4 +22,5 @@ textbox.addEventListener("keydown", (event) => {
 textbox.addEventListener("input", () => {
   localStorage.setItem("key", textbox.value);
   textbox.style.height = "auto";
+  textbox.style.height = textbox.scrollHeight + "px";
 });
